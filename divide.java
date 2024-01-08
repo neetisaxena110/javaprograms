8713b30bfdc37dae766ee7ccd24961dd3e0fcbdf
neeti <neetisaxena110@gmail.com>
2024-01-08 22:47:18 +0530

    division commit

---
+++ divide.java
@@ -0,0 +1,12 @@
+class Divide {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // multiply two numbers
+    int divide = first / second;
+    System.out.println(+first+ " / " +second+ "=" + divide);
+  }
+}
