aee806dfeb8199d1e462cf06e5525a836ed0fd56
neeti <neetisaxena110@gmail.com>
2024-01-08 22:36:46 +0530

    multiply commit

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // multiply two numbers
+    int multiply = first * second;
+    System.out.println(+first+ " * " +second+ "=" + multiply);
+  }
+}
